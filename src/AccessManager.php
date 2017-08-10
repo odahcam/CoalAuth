8e2cd2a5de95351061df8e644e5383ab4f26c7ec
Luiz Machado <machado@odahcam.com>
2017-08-10 13:47:07 -0300

    first files

---
+++ src/AccessManager.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace \CoalAuth;
+
+class AccessManager
+{
+    public function __construct()
+    {
+
+    }
+
+    public function login(User $user)
+    {
+
+    }
+
+    public function logout(User $user)
+    {
+        
+    }
+}
